989442ec2a0b015a254fad81dc0464849ef01fb4
Bubacarr Jobarteh <bubisjobs@gmail.com>
2019-07-16 12:53:02 +0000

    master blase setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,22 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+   mix.styles ([
+        'resources/sass/css/libs/blog-post.css',
+        'resources/sass/css/libs/bootstrap.css',
+        'resources/sass/css/libs/bootstrap.min.css',
+        'resources/sass/css/libs/font-awesome.css',
+        'resources/sass/css/libs/font-awesome.css',
+        'resources/sass/css/libs/metisMenu.css',
+        'resources/sass/css/libs/sb-admin-2.css',
+        'resources/sass/css/libs/styles.css',
+    ], 'public/css/app.css' );
+mix.scripts([
+    'resources/sass/js/libs/bootstrap.js',
+    'resources/sass/js/libs/bootstrap.min.js',
+    'resources/sass/js/libs/jquery.js',
+    'resources/sass/js/libs/bootstrap.js',
+     'resources/sass/js/libs/metisMenu.js',
+     'resources/sass/js/libs/sb-admin.js',
+     'resources/sass/js/libs/scripts.js',
+], 'public/js/app.js' );
